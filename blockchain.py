8298cba9118a95e42a0c339cafcd6cb4f4714ae3
Victor Khomyakov <homyakov.vs@edu.spbstu.ru>
2023-04-06 19:10:41 +0300

    Resolve #2

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,13 @@
+import argparse
+import asyncio
+import concurrent
 import hashlib
+import json
 import random
 import string
 import time
+from enum import Enum
+import socket
 
 
 class Block:
@@ -14,24 +20,56 @@ class Block:
         self.nonce = nonce
 
 
+class MessageType(Enum):
+    ANNOUNCE_BLOCK = 0
+
 class Node:
 
-    def __init__(self, genesis, total_blocks=10, timeout=120):
+    def __init__(self, port, genesis, total_blocks=10, timeout=120):
+        self.port = port
         self.genesis = genesis
         self.total_blocks = total_blocks
         self.timeout = timeout
 
         self.blockchain = []
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('localhost', self.port))
+        self.sock.setblocking(False)
+        self.interrupt = False
+
         self.init_time = time.perf_counter()
 
+    def listen(self):
+        while True:
+            try:
+                data, addr = self.sock.recvfrom(512)
+                msg = json.loads(data.decode())
+                if msg.get('type') == MessageType.ANNOUNCE_BLOCK.value:
+                    block = Block(**msg.get('block'))
+                    if self.validate_block(block):
+                        self.interrupt = True
+                        self.blockchain.append(block)
+
+                if len(self.blockchain) >= self.total_blocks or time.perf_counter() - self.init_time >= self.timeout:
+                    break
+            except BlockingIOError:
+                pass
 
     def work(self):
         while True:
             block = self.mine_block()
-
             if block is not None:
                 if self.validate_block(block):
                     self.blockchain.append(block)
+                    msg = {
+                        'type': MessageType.ANNOUNCE_BLOCK.value,
+                        'block': block.__dict__
+                    }
+                    data = json.dumps(msg).encode()
+                    for port in (10001, 10002, 10003):
+                        if self.port != port:
+                            self.sock.sendto(data, ('localhost', port))
+
             if len(self.blockchain) >= self.total_blocks or time.perf_counter() - self.init_time >= self.timeout:
                 return self.blockchain
 
@@ -50,6 +88,9 @@ class Node:
         data = ''.join(random.choices(string.ascii_letters+string.digits, k=256))
         nonce = 0
         while True:
+            if self.interrupt:
+                self.interrupt = False
+                return None
             nonce += random.randint(0, 10_000_000)
             hash = hashlib.sha256((str(index) + prev_hash + data + str(nonce)).encode()).hexdigest()
             if int(hash[-5:], 16) == 0:
@@ -61,3 +102,25 @@ class Node:
                     nonce=nonce
                 )
                 return block
+
+async def run_node(node):
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+    loop = asyncio.get_event_loop()
+    await asyncio.wait(
+        fs={
+            loop.run_in_executor(executor, node.listen),
+            loop.run_in_executor(executor, node.work)
+        }
+    )
+    node.sock.close()
+    return node.blockchain
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port')
+    parser.add_argument('--genesis')
+    kwargs = vars(parser.parse_args())
+    node = Node(int(kwargs['port']), int(kwargs['genesis']))
+    asyncio.run(run_node(node))
+    node.sock.close()
